68b8f975ed6d17abdcc498ec507ef6573cc78624
Ty Overby <ty@pre-alpha.com>
2015-10-26 17:34:21 -0700

    only sample at points inside the bounding boxes

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,31 @@ use implicit::*;
 
 struct ImplicitCanvas {
     draw_scale: f32,
-    size: (u32, u32),
     resolution: u32,
 }
 
 impl ImplicitCanvas {
-    fn sampling_points(&self) -> vec::IntoIter<(f32, f32)> {
-        let mut x = 0;
-        let mut y = 0;
+    fn sampling_points(&self, shape: &Implicit) -> vec::IntoIter<(f32, f32)> {
+        let bounding_box = shape.bounding_box().unwrap();
+        let start = bounding_box.top_left;
+        let end = bounding_box.bottom_right;
+        let start_x = (start.x / self.resolution as f32) as u32 * self.resolution;
+        let start_y = (start.y / self.resolution as f32) as u32 * self.resolution;
+        let end_x = (end.x / self.resolution as f32) as u32 * self.resolution;
+        let end_y = (end.y / self.resolution as f32) as u32 * self.resolution;
+
+
+        let mut x = start_x;
+        let mut y = start_y;
         let mut out = vec![];
         let res_scale = self.resolution as f32 * 0.5;
 
-        while y < self.size.1 {
-            while x < self.size.0 {
+        while y < end_y {
+            while x < end_x {
                 out.push((x as f32 + res_scale, y as f32 + res_scale));
                 x += self.resolution;
             }
-            x = 0;
+            x = start_x;
             y += self.resolution;
         }
         out.into_iter()
@@ -40,7 +48,7 @@ impl ImplicitCanvas {
     }
 
     fn render_pix<S: Implicit>(&self, shape: &S, frame: &mut Frame) {
-        for (sx, sy) in self.sampling_points() {
+        for (sx, sy) in self.sampling_points(shape) {
             let (dx, dy, ds) = self.sample_to_draw((sx, sy));
             let sample = shape.sample(Point { x: sx, y: sy } );
             frame.square(dx - 0.5 * ds, dy - 0.5 * ds, ds)
@@ -76,15 +84,31 @@ fn main() {
         right: circle_2
     };
 
+    let lone = Circle {
+        center: Point { x: 125.0, y: 200.0 },
+        radius: 50.0
+    };
+
+    struct Stripes;
+    impl Implicit for Stripes {
+        fn sample(&self, point: Point) -> f32 {
+            point.x.sin() * point.y.sin()
+        }
+
+        fn bounding_box(&self) -> Option<Rect> { None }
+    }
+
+    let lone_and_stripes = And { left: Stripes, right: lone };
+
     let mut canvas = ImplicitCanvas {
         draw_scale: 1.0,
-        size: (200, 200),
-        resolution: 4
+        resolution: 4,
     };
 
     while window.is_open() {
         let mut frame = window.cleared_frame(color::WHITE);
         canvas.render_pix(&anded, &mut frame);
+        canvas.render_pix(&lone_and_stripes, &mut frame);
 
         for event in window.events() {
             match event {
