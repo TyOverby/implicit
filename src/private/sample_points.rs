73fdba2a4295df7ed0386ccbd6b5d85a8d55d3f7
Ty Overby <ty@pre-alpha.com>
2016-04-08 08:42:59 -0700

    Switch to new point gatherer

--- src/private/sample_points.rs
+++ src/private/sample_points.rs
@@ -124,158 +124,21 @@ pub fn sampling_points<S: Implicit>(shape: &S, resolution: f32) -> Vec<(f32, f32
     let expand = b_dim * 0.10;
     let bb = bb.expand(expand, expand, expand, expand);
 
-    // Returns true if the entire rect should be added.
-    fn subdivide<S: Implicit>(shape: &S, bb:Rect, sample_dist: SampleDist, out: &mut Vec<Rect>) -> bool {
-        let radius = bb.width().max(bb.height());
-        let sample = shape.sample(bb.midpoint()).abs();
-
-        if sample > radius { return false; }
-        if radius < sample_dist.max_bump() * 10.0 || radius < 1.0 {
-            return true;
-        }
-
-        let q = bb.split_quad();
-        let a = subdivide(shape, q[0], sample_dist, out);
-        let b = subdivide(shape, q[1], sample_dist, out);
-        let c = subdivide(shape, q[2], sample_dist, out);
-        let d = subdivide(shape, q[3], sample_dist, out);
-
-        if a && b && c && d {
-            return true;
-        }
-
-        // TODO: this could be optimized by attempting to join neighboring
-        // rectangles
-
-        if a { out.push(q[0]) }
-        if b { out.push(q[1]) }
-        if c { out.push(q[2]) }
-        if d { out.push(q[3]) }
-
-        false
-    }
-
-    let mut rects = vec![];
+    ::flame::start("build poor mans quad tree");
     let sample_dist = SampleDist { x_bump: resolution, y_bump: resolution };
-    ::flame::start("gather rects");
-    if subdivide(shape, bb, sample_dist, &mut rects) {
-        rects.push(bb);
-    }
-    ::flame::end("gather rects");
-    println!("rects_len: {}", rects.len());
+    let pmqt = PoorMansQuadTree::build(shape, bb, sample_dist).unwrap();
+    ::flame::end("build poor mans quad tree");
 
-    ::flame::start("filter points");
     let mut out = vec![];
+    ::flame::start("filter points");
     for p in sample_from_box(bb, sample_dist) {
-        if rects.iter().any(|r| r.contains(&p)) {
+        if pmqt.contains(p) {
             out.push((p.x, p.y));
         }
     }
     ::flame::end("filter points");
 
     out
-}
-
-pub fn sampling_points_old<S: Implicit>(shape: &S, resolution: f32) -> Vec<(f32, f32)> {
-    let bb = shape.bounding_box().unwrap();
-    let b_dim = bb.width().max(bb.height());
-    let expand = b_dim * 0.10;
-    let bb = bb.expand(expand, expand, expand, expand);
-
-    assert!(!bb.is_null(), "shape is null");
-    let sample_dist = SampleDist {
-        x_bump: resolution,
-        y_bump: resolution,
-    };
-    let mut out = vec![];
-
-    fn subdivide<S: Implicit>(shape: &S, bb: Rect, sample_dist: SampleDist, out: &mut Vec<Point>) {
-        let radius = bb.width().max(bb.height());
-        let sample = shape.sample(bb.midpoint()).abs();
-
-        if sample > radius {
-            return
-        }
-        if  radius < sample_dist.max_bump() * 10.0 || radius < 1.0 {
-            out.extend(sample_from_box(bb, sample_dist));
-            return;
-        }
-
-        let q = bb.split_quad();
-        subdivide(shape, q[0], sample_dist, out);
-        subdivide(shape, q[1], sample_dist, out);
-        subdivide(shape, q[2], sample_dist, out);
-        subdivide(shape, q[3], sample_dist, out);
-    }
-
-    if shape.follows_rules() {
-        ::flame::start("subdividing");
-        subdivide(shape, bb, sample_dist, &mut out);
-        ::flame::end("subdividing");
-    } else {
-        out.extend(sample_from_box(bb, sample_dist));
-    }
-
-    ::flame::start("remove overlapping");
-    out.sort_by(|a, b| {
-        match a.x.partial_cmp(&b.x) {
-            Some(a) => a,
-            None => Ordering::Equal
-        }
-    });
-    remove_similar(&mut out);
-
-    out.sort_by(|a, b| {
-        match a.y.partial_cmp(&b.y) {
-            Some(a) => a,
-            None => Ordering::Equal
-        }
-    });
-    remove_similar(&mut out);
-    ::flame::end("remove overlapping");
-
-    // TODO: make this function return points
-    flame::span_of("conversion", || out.into_iter().map(|p| p.into_tuple()).collect())
-}
-
-fn remove_similar(out: &mut Vec<Point>) {
-    let mut last = None;
-    let mut to_remove: Vec<usize> = vec![];
-
-    // Build up a list of indices to remove.
-    for (i, &pt) in out.iter().enumerate() {
-        if last.is_none() {
-            last = Some(pt);
-            continue;
-        }
-        let last_u = last.unwrap();
-        if pt.close_to(&last_u, 0.01) {
-            to_remove.push(i);
-        } 
-        last = Some(pt);
-    }
-
-    // Reverse the list so that we can "pop" from the front
-    to_remove.reverse();
-
-    // Drop all the removed indicies
-    let mut i = 0;
-    out.retain(|_| {
-        if to_remove.is_empty() {
-            return true;
-        }
-
-        let &last_idx = to_remove.last().unwrap();
-        let result = if last_idx == i {
-            to_remove.pop();
-            false
-        } else {
-            true
-        };
-
-        i += 1;
-        result
-    });
 }
 
 fn sample_from_box(mut bb: Rect, sample_dist: SampleDist) -> BoxSampler {
